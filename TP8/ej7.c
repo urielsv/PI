cef6d76aa990f186e9b96102a1710de3c34feceb
Uriel <67885888+urielsv@users.noreply.github.com>
2023-05-10 19:32:02 -0300

    Update ej7.c
    Alojar memoria de forma exponencial en vez de por bloques.

--- TP8/ej7.c
+++ TP8/ej7.c
@@ -7,147 +7,109 @@
  *
  * (Mas info en el TP8).
  */
-
 #include <stdio.h>
-#include <string.h>
-#include <assert.h>
 #include <stdlib.h>
+#include <stdbool.h>
+#include <string.h>
 
-#define COLS 7
-#define FILS 6
+#define FILS 30
+#define COLS 30
 
+typedef enum {DER=0, IZQ, ABA, ARR, I_AR, I_AB, D_AR, D_AB } Tdireccion;
 
-typedef enum {DER=0, IZQ, ABA, ARR, I_AR, I_AB, D_AR, D_AB} Tdireccion;
-
-struct posicion {
+typedef struct posicion {
     char * palabra;
     size_t fila;
     size_t columna;
     Tdireccion direccion;
-};
+} Posicion;
 
-// NOTA: depende del orden en que realizan la busqueda, el testeo puede dar diferente
-//       en esta prueba asumimos que se recorre el diccionario, y cada palabra se busca en la sopa de 
-//       letras.
-//.      Si en vez de hacerlo así recorren la matriz y por cada letra buscan si hay una palabra en el diccionario
-//       que empiece con esa letra, tienen que cambiar el testeo para que coincida con el orden en que se encuentran.
-
-struct posicion * resolverSopa(char ** dic, char m[FILS][COLS]);
-int busqueda(char * palabra, char m[FILS][COLS],int i, int j, int dirs[]);
-int buscarDir(char * palabra, char m[FILS][COLS], int i, int j, int dirX, int dirY);
-
-int main(void) {
-  char * diccionario[] = {"ARRE", "CANCION", "CAPA", "ERROR", "ORCO", "PERRO", "PERTINAZ", "REA", "RIO", ""};
-   
-  char sopa[FILS][COLS] =
-     {{'X','X','X','O','X','X','X'},
-      {'A','P','A','C','Y','Y','O'},
-      {'Z','E','Z','R','Z','C','X'},
-      {'E','R','R','O','R','X','X'},
-      {'X','R','I','O','I','E','X'},
-      {'X','O','X','X','O','X','X'}};
-  
-  struct posicion * res = resolverSopa(diccionario, sopa);
-  // La cantidad de palabras encontradas debe ser 9
-  int expected = 9;
-  int count = 0;
-  while ( res[count].palabra != NULL)
-     printf("%s\n", res[count++].palabra);
-  assert(count == expected);
-
-  assert(strcmp(res[0].palabra,"ARRE")==0);
-  assert(res[0].fila==1);
-  assert(res[0].columna==2);
-  assert(res[0].direccion==D_AB);
-
-  assert(strcmp(res[3].palabra,"ORCO")==0);
-  assert(res[3].fila==3);
-  assert(res[3].columna==3);
-  printf("%d\n", res[3].direccion);
-  assert(res[3].direccion==ARR);
-  
-  assert(strcmp(res[4].palabra,"ORCO")==0);
-  assert(res[4].fila==4);
-  assert(res[4].columna==3);
-  assert(res[4].direccion==D_AR);
-
-  assert(strcmp(res[6].palabra,"REA")==0);
-  assert(res[6].fila==3);
-  assert(res[6].columna==2);
-  assert(res[6].direccion==I_AR);
-
-  puts("OK");
-  return 0;
+bool buscarDir(const char *palabra, char m[FILS][COLS], int i, int j, int row_delta, int col_delta) {
+    for (size_t k = 0; palabra[k]; k++) {
+        int row = i + k * row_delta;
+        int col = j + k * col_delta;
+        
+        if (row < 0 || row >= FILS || col < 0 || col >= COLS || m[row][col] != palabra[k]) {
+            return false;
+        }
+    }
+    return true;
 }
 
-#define BLOCK   5
-struct posicion * resolverSopa(char ** dic, char m[FILS][COLS])
-{
-    struct posicion * resp = NULL;
-    char * palabra;
-    int dirs[D_AB+1];
-    int ctr_dirs, dir;
-    int t = 0;
+int busqueda(const char *palabra, char m[FILS][COLS], int i, int j, Tdireccion dirs[]) {
+    int row_deltas[] = {0, 0, 1, -1, -1, 1, -1, 1};
+    int col_deltas[] = {1, -1, 0, 0, -1, -1, 1, 1};
 
-    for (int i = 0; dic[i][0]; i++) { // Check que no sea el string vacio.
-        palabra = dic[i]; // Primera palabra.
-        for (int j = 0; j < FILS; j++) {
-            for (int k = 0; k < COLS; k++) {
-               if (m[i][j] == palabra[0]) { // Si coinciden la primera entonces busco en direcciones.
-                    ctr_dirs = busqueda(palabra, m, j, k, dirs);
-                                           
-                    for (dir = 0; dir < ctr_dirs; dir++) {
-                        if (t % BLOCK == 0) {
-                            resp = realloc(resp, (t + BLOCK + 1) * sizeof(*resp));
+    int ctr = 0;
+
+    for (int k = 0; k < 8; k++) {
+        if (buscarDir(palabra, m, i, j, row_deltas[k], col_deltas[k])) {
+            dirs[ctr++] = k;
+        }
+    }
+
+    return ctr;
+}
+
+Posicion *resolverSopa(char m[FILS][COLS], char **dic) {
+    size_t capacity = 10;
+    Posicion *result = malloc(capacity * sizeof(Posicion));
+    if (!result) {
+        return NULL;
+    }
+    size_t size = 0;
+
+    for (size_t d = 0; dic[d][0]; d++) {
+        char *palabra = dic[d];
+
+        for (int i = 0; i < FILS; i++) {
+            for (int j = 0; j < COLS; j++) {
+                if (m[i][j] == palabra[0]) {
+                    Tdireccion dirs[8];
+                    int found = busqueda(palabra, m, i, j, dirs);
+
+                    for (int k = 0; k < found; k++) {
+                        /* Realojamos con crecimiento exponencial. */
+                        if (size >= capacity) {
+                            capacity *= 2;
+                            Posicion *new_result = realloc(result, 
+                                                    capacity * sizeof(Posicion));
+                            if (!new_result) {
+                                free(result);
+                                return NULL;
+                            }
+                            result = new_result;
                         }
-                        struct posicion ans = {palabra, i, j, dirs[dir]};
-                        resp[t++] = ans;
+                        /* Otra forma seria utilizar bloques tal que:
+                        if (size % BLOCK == 0) {
+                            capacity += BLOCK;
+                            Posicion *new_result = realloc(result, capacity * sizeof(Posicion));
+                            if (!new_result) {
+                                free(result);
+                                return NULL;
+                            }
+                            result = new_result;
+                        }
+                        El problema de esto es que si el bloque es demasiado 
+                        pequeno se harian demasiados realloc con mucha frecuencia.
+                        */
+                        result[size++] = (Posicion){.palabra = palabra, .fila = i, .columna = j, .direccion = dirs[k]};
                     }
-               }
+                }
             }
         }
     }
-    if (resp == NULL)
-        resp = malloc(sizeof(*resp));
-    struct posicion fin = {NULL, 0, 0, 0};
-    resp[t++] = fin;
-    resp = realloc(resp, t * sizeof(*resp));
-    
-    return resp;
-}
-#undef BLOCK
-int busqueda(char * palabra, char m[FILS][COLS],int i, int j, int dirs[])
-{
-    int ctr = 0;
-    if (buscarDir(palabra, m, i, j, 0, 1))
-       dirs[ctr++] == DER;
-    if (buscarDir(palabra, m, i, j, 0, -1))
-       dirs[ctr++] == IZQ;
-    if (buscarDir(palabra, m, i, j, 1, 0))
-       dirs[ctr++] == ABA;
-    if (buscarDir(palabra, m, i, j, -1, 0))
-       dirs[ctr++] == ARR;
-    if (buscarDir(palabra, m, i, j, -1, -1))
-       dirs[ctr++] == I_AR;
-    if (buscarDir(palabra, m, i, j, 1, -1))
-       dirs[ctr++] == I_AB;
-    if (buscarDir(palabra, m, i, j, -1, 1))
-       dirs[ctr++] == D_AR;
 
-   return ctr;
-
-}
-
-int buscarDir(char * palabra, char m[FILS][COLS], int i, int j, int dirX, int dirY)
-{
-    int len = strlen(palabra);
-    int k = 0;
-    for (; k < len && i >= 0 && j >=0 && i < FILS && j < COLS; k++) {
-        if (palabra[k] != m[i][j]) 
-            return 0;
-      // Vi por la solucion que se incrementa para el lado que estamos evaluando en la fn "busqueda".
-      i += dirX;
-      j += dirY;
+    // Añadir el último elemento con palabra=NULL para marcar el final del vector
+    if (size >= capacity) {
+        Posicion *new_result = realloc(result, (++capacity) * sizeof(Posicion));
+        if (!new_result) {
+            free(result);
+            return NULL;
+        }
+        result = new_result;
     }
-    return k == len;
+    result[size] = (Posicion){.palabra = NULL, .fila = 0, .columna = 0, .direccion = 0};
+
+    return result;
 }
